0037610a22e900aaa825749a53ac35c0130f93ac
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-25 13:31:47 +0100

    Used `let` and `const` for variable declarations

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,13 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-<<<<<<< HEAD
     const task = true;
     const task2 = false;
-=======
-    let task = true;
-    let task2 = false;
->>>>>>> 7a2ba4606b241207a7612bc211ebb7d2252f269f
   }
 
   return [task, task2];
