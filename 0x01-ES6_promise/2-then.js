355aaa14b658119d9d6c38b8280f8d4d5fca510d
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-26 21:11:52 +0100

    Added third handler for rejected promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }), () => new Error());
-
+  promise.then(() => ({ status: 200, body: 'success' }));
+  promise.then(null, () => new Error());
   promise.finally(() => {
     console.log('Got a response from the API');
   });
