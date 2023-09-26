19b8e1d95035c0d64e0a6c8afa6c84ba2425a468
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-26 22:17:47 +0100

    used Promise.all().catch() to handle rejections

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,10 @@ export default function handleProfileSignup() {
   const promise2 = createUser();
 
   Promise.all([promise1, promise2])
-  .then((value) => {
-    console.log(`${value[0].status} ${value[1].firstName} ${value[1].lastName}`)
-  })
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+    .catch(() => {
+      console.error(new Error('Signup system offline'));
+    });
 }
