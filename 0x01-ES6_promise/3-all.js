5b0b12dfe9ff2e62a5687c2b6aa9de70c66c9a83
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-26 20:07:39 +0100

    Done with task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import {uploadPhoto, createUser} from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  promise1.then((value1) => {
+    promise2.then((value2) => {
+      console.log(`${value1.body} ${value2.firstName} ${value2.lastName}`);
+    }, (error) => {
+      console.error('Signup system offline');
+    });
+  }, (error) => {
+    console.error('Signup system offline');
+  });
+}
