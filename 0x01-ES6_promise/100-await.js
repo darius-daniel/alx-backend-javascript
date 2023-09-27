fd88504384c865f434db739622765abb3961ccef
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-27 14:55:47 +0100

    Added `onRejected` handler

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,6 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function asyncUploadUser() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => ({ photo: values[0], user: values[1] }))
+    .then((values) => ({ photo: values[0], user: values[1] }),
+      (values) => ({ photo: values[0], user: values[1] }))
     .catch(() => ({ photo: null, user: null }));
 }
