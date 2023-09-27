5f99da5e7c84d1cc022b328ed0664e198d204341
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-27 09:25:00 +0100

    Added catch() to Promise.all() statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,8 @@ export default function handleProfileSignup() {
   return Promise.all([promise1, promise2])
     .then((values) => {
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    }, () => {
+    })
+    .catch(() => {
       console.error(new Error('Signup system offline'));
     });
 }
