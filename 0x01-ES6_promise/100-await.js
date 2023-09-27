ab71877a7f40803b8ff5b66d45a443ac83d05740
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-27 15:11:05 +0100

    Massively overhauled code from previous push

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function asyncUploadUser() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => ({ photo: values[0], user: values[1] }))
-    .catch(() => ({ photo: null, user: null }));
+export default async function asyncUploadUser() {
+  let user, photo;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    return { photo: null, user: null };
+  } finally {
+    return ({ photo, user });
+  }
 }
