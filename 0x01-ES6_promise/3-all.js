cc0a683b16004ebf8c297d0cfa347e5f43e6c289
Darius Daniel <dariusdaniel771@gmail.com>
2023-09-27 09:21:55 +0100

    Added returned collectively resolve Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
 
-  Promise.all([promise1, promise2])
+  return Promise.all([promise1, promise2])
     .then((values) => {
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     }, () => {
