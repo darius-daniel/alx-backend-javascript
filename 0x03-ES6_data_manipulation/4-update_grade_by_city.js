eae8ec6e7c3b75df77e3b0050fd177503cc82a17
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-03 02:10:07 +0100

    Fixed styling errors in 2 scripts

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(array, city, newGrades) {
   const result = array.filter((item) => item.location === city);
   return result.map((item) => {
     if (!newGrades.filter((val) => val.grade).includes(item.grade)) {
-      item['grade'] = 'N/A';
+      item.grade = 'N/A';
     }
     return item;
   });
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw 'Positon outside range';
+    throw new Error('Positon outside range');
   } else {
     const buffer = new ArrayBuffer(length);
     const dataView = new DataView(buffer);
-    
+
     dataView.setInt8(position, value);
     return dataView;
   }
