1d93d0a228fa077fc36f4b877ef7fbab535185fa
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-20 13:31:03 +0100

    Pushing scripts for tasks 0  and 1 in new project directory

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+/// <reference path="./Teacher.ts" />
+namespace Subjects {
+  export class Subject {
+    teacher: Subjects.Teacher
+
+    set setTeacher(teacher: Subjects.Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
