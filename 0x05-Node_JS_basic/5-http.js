e8172c29e30c80520dd0597eb47fc0ca20626a21
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-22 02:16:09 +0100

    fixed eslint errors in script 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,14 +4,14 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer((request, response) => {
   const db = process.argv[2];
 
-  response.writeHead(200, { 'Content-Type': 'text/plain'});
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (request.url === '/students') {
-    let msg = 'This is a list of our students\n'
+    let msg = 'This is a list of our students\n';
     countStudents(db)
       .then((namesPerField) => {
-        const csStudents = namesPerField['CS'];
-        const sweStudents = namesPerField['SWE'];
+        const csStudents = namesPerField.CS;
+        const sweStudents = namesPerField.SWE;
         msg += `Number of students: ${csStudents.length + sweStudents.length}\n`;
         msg += `Number of students in CS: ${csStudents.length}. List: `;
 
@@ -20,9 +20,9 @@ const app = http.createServer((request, response) => {
           if (idx < csStudents.length - 1) {
             delimiter = ', ';
           }
-          msg +=`${csStudents[idx]}${delimiter}`;
+          msg += `${csStudents[idx]}${delimiter}`;
         }
-      
+
         msg += `Number of students in SWE: ${sweStudents.length}. List: `;
         for (const idx in sweStudents) {
           let delimiter = '\n';
@@ -36,11 +36,11 @@ const app = http.createServer((request, response) => {
       })
       .catch((error) => {
         response.end(error.toString());
-      })
+      });
   } else {
     response.end('Hello Holberton School!');
   }
-})
+});
 
 app.listen(1245, '127.0.0.1');
 
