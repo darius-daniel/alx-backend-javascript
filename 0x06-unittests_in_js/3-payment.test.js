f11ed421c4af61dcae94e9807898934143cce8eb
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-23 11:55:20 +0100

    Refactored code 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,13 +3,20 @@ const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
-describe("sendPaymentRequestToApi", () => {
-  it("Validate the usage of the Utils function", () => {
-    const spy = sinon.spy(Utils);
+describe("sendPaymentRequestToApi", function () {
+  const sandbox = sinon.createSandbox();
 
+  beforeEach(function () {
+    sandbox.spy(Utils); 
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it("Validate the usage of the Utils function", function () {
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(1);
-    spy.calculateNumber.restore
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
   });
 });
